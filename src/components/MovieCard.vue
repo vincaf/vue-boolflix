684a6cc14a673f70e30d38af73fc558de8a2358b
vincaf <vincenzocafassog@gmail.com>
2022-07-25 18:45:44 +0200

    added first 5 actors of the cast

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -2,6 +2,7 @@
   <div id="MovieCard" class="col-2">
     <img class="w-100" v-if="movie.poster_path == null || movie.poster == ''" src="http://www.movienewz.com/img/films/poster-holder.jpg" :alt="movie.title">
     <img class="w-100" v-else :src="`${this.imgUrl}${movie.poster_path}`" :alt="movie.title">
+    <div id="retroCard">
     <ul class="list-unstyled">
         <li><strong>Titolo:</strong> {{ movie.title }}</li>
         <li><strong>Titolo originale:</strong> {{ movie.original_title }}</li>
@@ -16,12 +17,18 @@
         </li>
         <li v-else><strong>Voto: </strong> non disponibile</li>
         <li><strong>Panoramica:</strong> {{ movie.overview }}</li>
+        <div class="text-center">
+            <button @click="getCast(movie.id)" class="my-2 btn btn-outline-secondary">Scopri il cast</button>
+            <li v-for="member in cast" :key="member.id" class="mt-2"> {{member.name}} </li>
+        </div>
     </ul>
+    </div>
   </div>
 </template>
 
 <script>
 import LangFlag from 'vue-lang-code-flags';
+import axios from 'axios';
 
 export default {
     name: 'MovieCard',
@@ -37,6 +44,10 @@ export default {
         supportedLanguage: [
                 'am', 'az', 'be', 'bn', 'bg', 'zh', 'ca', 'cs', 'en', 'et', 'fr', 'de', 'ha', 'hi', 'hu', 'it', 'ja', 'jv', 'km', 'ko', 'lv', 'ms', 'mr', 'fa', 'pl', 'pt', 'ro', 'ru', 'es', 'sw', 'ta', 'te', 'th', 'tr', 'uz', 'vi',
             ],
+
+        cast: [],
+        apiKey: '88ad43231db09cad59cba4c08bf2d037',
+        apiUrl: 'https://api.themoviedb.org/3/movie/',
         }
     },
 
@@ -50,7 +61,19 @@ export default {
     methods:{
         getIntegerVote(vote){
             return Math.round(vote / 2);
-        }
+        },
+
+        getCast(id){
+            axios.get(`${this.apiUrl}${id}/credits?api_key=${this.apiKey}`)
+            .then(result => {
+                this.cast = result.data.cast;
+                this.cast.splice(5);
+                console.log(this.cast);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+        },
     }
 }
 </script>
@@ -73,6 +96,10 @@ export default {
         li{
             margin: 10px 0;
         }
+    }
+
+    button{
+        font-size: 14px;
     }
 
     #MovieCard{
